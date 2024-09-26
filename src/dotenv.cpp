e4d4ce81daeb8bf596426964ded6b18046e86e98
Emiliano Borghi <emiliano@smartmachine.nz>
2024-09-26 12:31:40 +1200

    Fix everything
    Signed-off-by: Emiliano Borghi <emiliano@smartmachine.nz>

--- src/dotenv.cpp
+++ src/dotenv.cpp
@@ -16,12 +16,11 @@
 
 namespace dotenv
 {
-    using char_buf = std::array<char, 255>;
     namespace {
         std::vector<std::string> env_vars{};
     }
 
-    std::pair<std::string, std::string> parse_line(const char_buf& line)
+    std::pair<std::string, std::string> parse_line(const std::string& line)
     {
         auto delim_pos = std::find(
             std::cbegin(line),
@@ -36,38 +35,10 @@ namespace dotenv
             '\0'
         );
 
-        std::string name{std::cbegin(line), delim_pos};
-        std::string value{delim_pos + 1, last_char};
+        const std::string name{std::cbegin(line), delim_pos};
+        const std::string value{delim_pos + 1, last_char};
 
-        for (auto it = std::find(std::begin(value), std::end(value), '$'); 
-                  it != std::cend(value);
-                  it = std::find(it + 1, std::end(value), '$'))
-        {
-            auto end_pos = std::find_if_not(
-                it + 1,
-                std::end(value),
-                ::isalnum
-            );
-
-            std::string val{it, end_pos};
-            auto env_val = dotenv::get_env(val.substr(1));
-            if (env_val == std::nullopt) 
-            {
-                for (auto pos = 0; (pos = value.find(val, pos) + 1); )
-                {
-                    value.replace(--pos, val.size(), "");
-                }
-            }
-            else
-            {
-                for (auto pos = 0; (pos = value.find(val, pos) + 1); pos += env_val.value().size())
-                {
-                    value.replace(--pos, val.size(), env_val.value());
-                }
-            }
-        }
-
-        return {std::move(name), std::move(value)};
+        return {name, value};
 
     }
 
@@ -77,33 +48,20 @@ namespace dotenv
 
         if (ifs.bad()) throw std::runtime_error("Could not load .env file!");
 
-        char_buf line{0};
-        int line_n = 1;
-        while (ifs.getline(std::begin(line), line.size()))
+        std::string line;
+        while (std::getline(ifs, line))
         {
             try
             {
                 // Skip empty lines and comments
+                if (line.empty()) continue;
                 if (line[0] == '#') continue;
                 if (line[0] == '\n') continue;
-                if (line.empty()) continue;
                 
-                auto env_vals = parse_line(line);
+                const auto env_vals = parse_line(line);
                 if (env_vals.second == "") continue;
-                setenv(env_vals.first.c_str(), env_vals.second.c_str(), 1);
-                auto it = std::find(
-                    std::begin(env_vars),
-                    std::end(env_vars),
-                    env_vals.first
-                );
-                if (it != std::end(env_vars))
-                {
-                    *it = std::move(env_vals.first);
-                } else 
-                {
-                    env_vars.emplace_back(std::move(env_vals.first));
-                }
-                std::cout << "Loaded " << env_vals.first << " = " << env_vals.second << "\n";
+                setenv(env_vals.first.c_str(), env_vals.second.c_str(), 1 /* overwrite */);
+                env_vars.emplace_back(env_vals.first);
             }
             catch(const std::runtime_error& e)
             {
